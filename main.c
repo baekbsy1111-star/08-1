c74131a18c5ccbbe4a38edc52fb8687252d1f06e
baekbsy1111-star <baekbsy1111@gmail.com>
2025-10-30 16:26:40 +0900

    Update main.c

--- main.c
+++ main.c
@@ -1,42 +1,19 @@
 #include <stdio.h>
-#include <stdlib.h>
-#define ROWS 3
-#define COLS 3
 
-void addMatrix(int A[][COLS], int B[][COLS], int C[][COLS])
+int main(void)
 {
-    int i, j;
-    for (i = 0; i < ROWS; i++)
-        for (j = 0; j < COLS; j++)
-            C[i][j] = A[i][j] + B[i][j];
-}
+    int i;
+    int grade[5];   
 
-void printMatrix(int A[][COLS])
-{
-    int i, j;
-    for (i = 0; i < ROWS; i++)
-    {
-        for (j = 0; j < COLS; j++)
-            printf("%4d", A[i][j]);
-        printf("\n");
-    }
-}
+    grade[0] = 10;
+    grade[1] = 20;
+    grade[2] = 30;
+    grade[3] = 40;
+    grade[4] = 50;
 
-int main(int argc, char *argv[])
-{
-    int A[ROWS][COLS] = {
-        {2, 3, 0},
-        {8, 9, 1},
-        {7, 0, 5}};
-    int B[ROWS][COLS] = {
-        {1, 0, 0},
-        {0, 1, 0},
-        {0, 0, 1}};
-    int C[ROWS][COLS];
-
-    addMatrix(A, B, C);
-    printMatrix(C);
-
+    for (i = 0; i < 5; i++)
+        printf("grade[%d] = %d\n", i, grade[i]);
+        
     system("PAUSE");
     return 0;
 }
