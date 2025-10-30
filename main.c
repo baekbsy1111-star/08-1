42d2b729971b8b521f2dc78ba339b057b618b40d
baekbsy1111-star <baekbsy1111@gmail.com>
2025-10-30 15:57:54 +0900

    Update main.c

--- main.c
+++ main.c
@@ -1,20 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#define SIZE 5
 
 int main(void)
 {
 int i;
-int grade[5];
-grade[0] = 10;
-grade[1] = 20;
-grade[2] = 30;
-grade[3] = 40;
-grade[4] = 50;
-printf("5명의 점수는 다음과 같습니다:\n");
-
+int grade[SIZE];
+int score[SIZE];
     for (i = 0; i < 5; i++)
-        printf("%d번째 학생 점수: %d\n", i + 1, grade[i]);
+    {
+    grade[i] = rand()%100;
+}
+ for (i = 0; i < 5; i++)
+ {
+     score[i]=grade[i];
+}
+
+     for (i = 0; i < 5; i++)
+     printf("score[%d]=%d\n", i, score[i]); 
+       
   system("PAUSE");	
   return 0;
 }
